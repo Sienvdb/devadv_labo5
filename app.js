7ffc079aaf3553dacd6de9347ddf3e7b8284a2bb
Sienvdb <sienvdbergh@gmail.com>
2022-04-28 15:52:01 +0200

    feat: make first getAll request

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/message');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/v1/message', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler

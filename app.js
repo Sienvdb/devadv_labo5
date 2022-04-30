0911a22d6817229b28f128fe10c08cac56753616
Sienvdb <sienvdbergh@gmail.com>
2022-04-30 13:30:19 +0200

    feat: add global get request

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/message');
-var usersRouter = require('./routes/users');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/v1/message', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/messages', indexRouter);
+
+app.get('/', (req, res) => {
+  res.render("index", {title: "This is a very cool message app!", message: "My first message"})
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
